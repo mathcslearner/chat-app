b507a88a5b98d2423458ae2e384b62cfe17cc822
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2025-11-01 16:51:08 -0400

    initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,37 @@
+import "dotenv/config"
+import express, {Request, Response} from "express"
+import cookieParser from "cookie-parser";
+import cors from "cors"
+import {Env} from "./config/env.config"
+import { HTTPSTATUS } from "./config/http.config";
+import { asyncHandler } from "./middlewares/asyncHandler.middleware";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import connectDatabase from "./config/database.config";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser())
+app.use(express.urlencoded({extended: true}));
+app.use(
+    cors({
+        origin: Env.FRONTEND_ORIGIN,
+        credentials: true
+    })
+)
+
+app.get("/health", 
+    asyncHandler(async (req: Request, res: Response) => {
+        res.status(HTTPSTATUS.OK).json({
+            message: "Server is healthy",
+            status: "OK"
+        })
+    })
+)
+
+app.use(errorHandler)
+
+app.listen(Env.PORT, async () => {
+    await connectDatabase();
+    console.log(`Server running on port ${Env.PORT} in ${Env.NODE_ENV} mode`)
+})

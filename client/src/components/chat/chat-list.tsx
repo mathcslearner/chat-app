396e09bb7df6b03d8a2b57267c16a24dff19d4f1
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2025-11-09 19:14:50 -0500

    chatlist items

--- client/src/components/chat/chat-list.tsx
+++ client/src/components/chat/chat-list.tsx
@@ -1,8 +1,37 @@
+import { useChat } from "@/hooks/use-chat";
+import { useEffect } from "react";
+import { Spinner } from "../ui/spinner";
+import ChatListItem from "./chat-list-item";
 
 
 const ChatList = () => {
+    const {fetchChats, chats, areChatsLoading} = useChat();
+
+    useEffect(() => {
+        fetchChats();
+    }, [fetchChats])
+    
     return (
-        <div>ChatList</div>
+        <div className="fixed inset-y-0 *:pb-20 lg:pb-0 lg:max-w-[379px] lg:block border-r border-border bg-sidebar max-w-[calc(100%-40px)] w-full left-10 z-[98]">
+            <div className="flex-col">
+                <div className="flex-1 h-[calc(100vh-100px)] overflow-y-auto">
+                    <div className="px-2 pb-10 pt-1 space-y-1">
+                        {areChatsLoading ? (
+                            <div className="flex items-center justify-center">
+                                <Spinner className="w-7 h-7"/>
+                            </div>
+                        ) : chats?.length === 0 ? (
+                            <div className="flex items-center justify-center">
+                                No chats created
+                            </div>
+                        ) : (chats?.map((chat) => (
+                            <ChatListItem key={chat._id} chat={chat} onClick={() => null}/>
+                        )))
+                        }
+                    </div>  
+                </div>
+            </div>
+        </div>
     )
 }
 
--- client/src/lib/helper.ts
+++ client/src/lib/helper.ts
@@ -1,5 +1,6 @@
 import { useSocket } from "@/hooks/use-socket";
-
+import type { ChatType } from "@/types/chat.type";
+import {format, isToday, isYesterday, isThisWeek} from "date-fns"
 
 export const isUserOnline = (userId?: string) => {
     if (!userId) {
@@ -9,4 +10,42 @@ export const isUserOnline = (userId?: string) => {
     const {onlineUsers} = useSocket.getState();
 
     return onlineUsers.includes(userId);
+}
+
+export const getOtherUserAndGroup = (chat: ChatType, currentUserId: string | null) => {
+    const isGroup = chat?.isGroup;
+
+    if (isGroup) {
+        return {
+            name: chat.groupName || "Unnamed Group",
+            subheading: `${chat.participants.length} members`,
+            avatar: "",
+            isGroup
+        }
+    }
+
+    const other = chat?.participants.find((p) => p._id !== currentUserId)
+    const isOnline = isUserOnline(other?._id ?? "")
+
+    return {
+        name: other?.name || "Unknown",
+        subheading: isOnline ? "Online" : "Offline",
+        avatar: other?.avatar || "",
+        isGroup: false,
+        isOnline
+    }
+}
+
+export const formatChatTime = (date: string | Date) => {
+    if (!date) return ""
+
+    const newDate = new Date(date)
+
+    if(isNaN(newDate.getTime())) return "Invalid date"
+
+    if (isToday(newDate)) return format(newDate, "h:mm a")
+    if (isYesterday(newDate)) return "Yesterday"
+    if (isThisWeek(newDate)) return format(newDate, "EEEE")
+
+    return format(newDate, "M/d")
 }
